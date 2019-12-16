d21c4e694a1cd4773c6c45d451cec62cd9b773ff
Goliath123 <esbenfx@gmail.com>
2019-12-16 12:38:29 +0100

    basics floor etc

---
+++ Algorithm/Components/FontRenderer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class FontRenderer : Component
+    {
+        string stringContent = "";
+        private SpriteFont font;
+
+        private Color renderColor = Color.Yellow;
+
+        public Color RenderColor { get => renderColor; set => renderColor = value; }
+
+        public FontRenderer(string content)
+        {
+            stringContent = content;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            font = content.Load<SpriteFont>("font");
+        }
+
+        public void SetContent(string content)
+        {
+            stringContent = content;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 tempOffset = new Vector2(-16, -16);
+            spriteBatch.DrawString(font, stringContent, Parent.Transform.Position + tempOffset, renderColor, 0f, Vector2.Zero, 1f, SpriteEffects.None, layer);
+        }
+    }
+}
--- Algorithm/Components/Transform.cs
+++ Algorithm/Components/Transform.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,52 @@ namespace Algorithm
 {
     public class Transform : Component
     {
+        Vector2 position;
+        Point gridPos;
+        private int tileSize = 32;
+
+        
+
+        public Vector2 Position
+        {
+            get
+            {
+                {
+                    return position;
+                }
+            }
+            set
+            {
+                {
+                    position = value;
+                    gridPos = new Point((int)position.X / tileSize, (int)position.Y / tileSize);
+                }
+            }
+        }
+        public Point GridPos
+        {
+            get
+            {
+                {
+                    return gridPos;
+                }
+            }
+            set
+            {
+                {
+                    gridPos = value;
+                    position = new Vector2((tileSize / 2) + gridPos.X * tileSize, (tileSize / 2) + gridPos.Y * tileSize);
+                }
+            }
+        }
+
+        public Transform(Vector2 transform)
+        {
+
+        }
+        public void Translate(Vector2 translation)
+        {
+
+        }
     }
 }

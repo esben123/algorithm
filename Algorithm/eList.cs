4050b1eacb93f75ee28c8e06fbd7aad4b6598d68
Goliath123 <esbenfx@gmail.com>
2019-12-16 10:25:53 +0100

    project files

---
+++ Algorithm/eList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    
+    public class eList<T>
+    {
+        private class GenericsNode
+        {         
+            private GenericsNode next;
+            private T data;
+
+            public GenericsNode Next { get => next; set => next = value; }
+            public T Data { get => data; set => data = value; }
+
+
+            public GenericsNode(T t)
+            {
+                next = null;
+                data = t;
+            }
+        }
+
+        private GenericsNode head;
+
+        public eList()
+        {
+            head = null;
+        }
+
+        public void Add(T t)
+        {
+            GenericsNode n = new GenericsNode(t);
+            n.Next = head;
+            head = n;
+        }
+
+        public void Remove(T t)
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            GenericsNode current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}

7670e4d2cb7dd5c55ea21aed47c3b0c0a5303e35
Goliath123 <esbenfx@gmail.com>
2019-12-16 20:26:52 +0100

    random items added to shoppingList

--- Algorithm/Components/ShoppingList.cs
+++ Algorithm/Components/ShoppingList.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
-    public class ShoppingList : Component
+    public enum Item
     {
+        Spear = 0,
+        Armour = 1,
+        Helmet = 2,
+        Necronomicon = 3,
+        CallOfCthulhu = 4
+    }
+
+        public class ShoppingList : Component
+    {
+        private Item item;
+        private List<Item> itemsToPurchase = new List<Item>();
+        private Random rnd = new Random(Environment.TickCount);
+        private int itemIndex;
+        private int nrOfItems;
+
+        public ShoppingList()
+        {
+            nrOfItems = rnd.Next(1, 4);
+
+            for (int i = 0; i < nrOfItems; i++)
+            {
+                itemIndex = rnd.Next(0, 5);
+                item = (Item)itemIndex;
+                itemsToPurchase.Add(item);
+            }
+            foreach (Item item in itemsToPurchase)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
     }
 }

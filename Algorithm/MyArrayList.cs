8615bc35b3f2912cf659795ff46462d62c138a50
Sindri Berg <sindrinr_1@hotmail.com>
2019-12-17 19:59:58 +0100

    LinkedList
    No more arraylist.
    Using linkedlist instead. Also started smart linkedlist, and sorted.

--- Algorithm/MyArrayList.cs
+++ Algorithm/MyArrayList.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +16,8 @@ namespace Algorithm
         /// </summary>
         public MyArrayList()
         {
-            this.currentSize = 10;
-            this.items = new T[this.currentSize];
+            currentSize = 10;
+            items = new T[this.currentSize];
         }
 
         /// <summary>
@@ -27,11 +26,12 @@ namespace Algorithm
         /// <param name="item">Generic item.</param>
         public void Append(T item)
         {
-            //If the index is smaller than the size of the list
+            //If the index is smaller than the size of the array
             if (currentIndex < currentSize)
             {
-                this.items[currentIndex] = item;
-                this.currentIndex++;
+                items[currentIndex] = item;
+                currentIndex++;
+                //We add to the end of the array at the current index, and we increase the index.
             }
             //If the index is is not smaller than the size, we increase
             //the size of the list by the increaseSize variable.
@@ -52,15 +52,22 @@ namespace Algorithm
 
         public void Prepend(T item)
         {
-            if (currentIndex < currentSize)
-            {
-                this.items[0] = item;
-                this.currentIndex++;
-            }
+            //To prepend on array, we must move the entire array up one slot. 
+            //This requires us to make a new array, and copy old one into the new one.
+            //Arrays are immutable, so you can't move items in the array. 
+            //We then set the new array to be our current array.
+            var newSize = currentSize * increaseSize;
+            var newArray = new T[newSize];
+            newArray[0] = item;
+            items.CopyTo(newArray, 1);
+            items = newArray;
+
         }
 
-        public static T[] RemoveAt(this T[] source, int index)
+
+        public T[] RemoveAt(T[] source, int index)
         {
+            source = items;
             //We create a new array, that is 1 item smaller.
             T[] dest = new T[source.Length - 1];
 
@@ -80,12 +87,23 @@ namespace Algorithm
         }
         public void RemoveFirst(T item)
         {
+            //To remove the first item of the array, we have to create a new array.
+            //We then move the old one into the new, but start at index 1, so the first item does not get transferred.
+            //We then set our newarray to be our current.
+            items[0] = item;
+            var newArray = new T[currentSize - 1];
+            items.CopyTo(newArray, 1);
+            items = newArray;
             
         }
 
-        public void RemoveLast(T item)
+        public void RemoveLast()
         {
-
+            Array.Resize(ref items, items.Length - 1);
+            //To remove the last item of an array, we can use this method to resize it.
+            //Then we don't need to know anything about the last item, since the item will get removed when we resize it. 
         }
+
+
     }
 }

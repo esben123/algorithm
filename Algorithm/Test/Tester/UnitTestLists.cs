4bfb9369daad4ec5e9520559007b28fa02e0edb5
ThostDk <thomaso.work@gmail.com>
2019-12-18 13:41:39 +0100

    glemte at save alt

--- Algorithm/Test/Tester/UnitTestLists.cs
+++ Algorithm/Test/Tester/UnitTestLists.cs
@@ -5,7 +5,7 @@ using Algorithm;
 namespace Tester
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestLists
     {
         //Test standard linked list
         [TestMethod]
@@ -31,40 +31,6 @@ namespace Tester
             //Act
             //Assert
         }
-        //Test if AI can get from point A to B  
-        [TestMethod]
-        public void TestAstarBasic()
-        {
-            //Arrange
-            //Act
-            //Assert
-        }
-
-        //Test wether AI chooses the shortest path  
-        [TestMethod]
-        public void TestAstarPathOptimization()
-        {
-            //Arrange
-            //Act
-            //Assert
-        }
-
-        //Test if AI can go from point A to B and then continue from point B to C  
-        [TestMethod]
-        public void TestAstarMultiplePaths()
-        {
-            //Arrange
-            //Act
-            //Assert
-        }
-
-        //Test AI with blocked path  
-        [TestMethod]
-        public void TestAstarNoPath()
-        {
-            //Arrange
-            //Act
-            //Assert
-        }
+        
     }
 }
